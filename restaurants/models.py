a4dbf55b74d54c1d1de39815d4f6b408818bea31
Artem Marhaza <artemmrgz@gmail.com>
2022-07-11 18:11:14 +0200

    mute linter warnings

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,4 @@
+# pylint: disable=missing-module-docstring, unused-import
 from django.db import models
 
 # Create your models here.
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,4 @@
+# pylint: disable=missing-module-docstring, unused-import
 from django.shortcuts import render
 
 # Create your views here.
