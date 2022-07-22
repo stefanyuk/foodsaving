e0a30b784a82e241e73787ed7bf3d76f924b1096
Artem Marhaza <artemmrgz@gmail.com>
2022-07-22 12:28:13 +0200

    add restaurant model

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,12 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    """Schema to represent restaurant attributes in the database table."""
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, blank=False, null=False)
+    phone_number = models.CharField(max_length=15, verbose_name="phone number")
+    email = models.EmailField(unique=True, verbose_name="email address")
+    description = models.TextField()
